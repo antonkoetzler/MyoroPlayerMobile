0adf79147374d50052628b7496d58d7bb27359a2
Anton Koetzler-Faust <antonkoetzler-faust@hotmail.com>
2022-07-03 01:06:27 -0600

    Adding me work

---
+++ MyoroPlayer/src/MenuButton.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  TouchableOpacity,
+  Image,
+  Dimensions
+} from 'react-native';
+
+const MenuButton = ({style, src, onPress}) => (
+  <TouchableOpacity style={style}>
+    <Image
+      source={{ uri: src }}
+      style={{
+        width: (Dimensions.get("window").width / 4) - 30,
+        height: (Dimensions.get("window").width / 4) - 30,
+        alignSelf: "center"
+      }}
+    />
+  </TouchableOpacity>
+);
+
+export default MenuButton;
